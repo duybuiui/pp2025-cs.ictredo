a1ab1cf34e8266cef77897509386d6c0bf3b15b1
duybuiui <vandunghoi23@gmail.com>
2025-01-03 06:03:02 -0800

    course.py of pw6

---
+++ pw6/domains/course.py
@@ -0,0 +1,7 @@
+from domains.BasicInfo import BasicInfo
+from input import new_course
+class Courses:
+    def __init__(self):
+        self.courses: list[BasicInfo] = []
+    def new_course(self):
+        new_course(self.courses)
